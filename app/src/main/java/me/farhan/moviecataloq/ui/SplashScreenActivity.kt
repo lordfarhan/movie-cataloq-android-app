b115992d318bae478d045e6a1bf76695cd154768
Muhammad Farhan Abdur Rosyid <farhan1205muhammad@gmail.com>
2021-01-11 23:06:24 +0700

    Implemented reactive programming in splashscreen.

--- app/src/main/java/me/farhan/moviecataloq/ui/SplashScreenActivity.kt
+++ app/src/main/java/me/farhan/moviecataloq/ui/SplashScreenActivity.kt
@@ -2,9 +2,12 @@ package me.farhan.moviecataloq.ui
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import me.farhan.moviecataloq.R
 import me.farhan.moviecataloq.core.util.SPLASH_SCREEN_DURATION
 import me.farhan.moviecataloq.databinding.ActivitySplashScreenBinding
@@ -13,14 +16,17 @@ import me.farhan.moviecataloq.ui.landing.LandingActivity
 class SplashScreenActivity : AppCompatActivity() {
 
   private lateinit var binding: ActivitySplashScreenBinding
+  private val activityScope = CoroutineScope(Dispatchers.Main)
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     binding = DataBindingUtil.setContentView(this, R.layout.activity_splash_screen)
 
-    Handler().postDelayed({
-      startActivity(Intent(this, LandingActivity::class.java))
+    activityScope.launch {
+      delay(SPLASH_SCREEN_DURATION)
+
+      startActivity(Intent(this@SplashScreenActivity, LandingActivity::class.java))
       finish()
-    }, SPLASH_SCREEN_DURATION)
+    }
   }
 }
