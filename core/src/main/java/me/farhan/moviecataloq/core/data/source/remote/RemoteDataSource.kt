2a20e2e38d96a21dd43b1d34d0221b07bd295b3b
Muhammad Farhan Abdur Rosyid <farhan1205muhammad@gmail.com>
2021-01-31 10:56:11 +0700

    Removed unused libraries.

--- core/src/main/java/me/farhan/moviecataloq/core/data/source/remote/RemoteDataSource.kt
+++ core/src/main/java/me/farhan/moviecataloq/core/data/source/remote/RemoteDataSource.kt
@@ -10,7 +10,6 @@ import me.farhan.moviecataloq.core.data.source.remote.network.ApiService
 import me.farhan.moviecataloq.core.data.source.remote.response.ApiResponse
 import me.farhan.moviecataloq.core.data.source.remote.response.MovieResponse
 import me.farhan.moviecataloq.core.data.source.remote.response.TvShowResponse
-import me.farhan.moviecataloq.core.util.EspressoIdlingResource
 
 /**
  * @author farhan
@@ -19,7 +18,6 @@ import me.farhan.moviecataloq.core.util.EspressoIdlingResource
 class RemoteDataSource(private val apiService: ApiService) {
 
   suspend fun fetchMovies(): Flow<ApiResponse<List<MovieResponse>>> {
-    EspressoIdlingResource.increment()
     return flow {
       try {
         val response = apiService.getPopularMovies(BuildConfig.API_KEY)
@@ -37,7 +35,6 @@ class RemoteDataSource(private val apiService: ApiService) {
   }
 
   fun fetchMovieDetail(id: Long): Flow<ApiResponse<MovieResponse>> {
-    EspressoIdlingResource.increment()
     return flow {
       try {
         val response = apiService.getMovieDetail(id, BuildConfig.API_KEY)
@@ -50,7 +47,6 @@ class RemoteDataSource(private val apiService: ApiService) {
   }
 
   fun fetchTvShows(): Flow<ApiResponse<List<TvShowResponse>>> {
-    EspressoIdlingResource.increment()
     return flow {
       try {
         val response = apiService.getPopularTvShow(BuildConfig.API_KEY)
@@ -68,7 +64,6 @@ class RemoteDataSource(private val apiService: ApiService) {
   }
 
   fun fetchTvShowDetail(id: Long): Flow<ApiResponse<TvShowResponse>> {
-    EspressoIdlingResource.increment()
     return flow {
       try {
         val response = apiService.getTvShowDetail(id, BuildConfig.API_KEY)
